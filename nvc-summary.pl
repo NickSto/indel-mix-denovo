e8d8784980762f58ffb174b3547d26def8d0ca58
me <nmapsy@gmail.com>
2013-12-05 11:08:52 -0500

    nvc-summary.pl: Added -F option for a filename column.

--- nvc-summary.pl
+++ nvc-summary.pl
@@ -20,6 +20,7 @@ sample columns of each line (Note: it combines all samples on each line).
   -p: Give more precise frequencies (two decimal points instead of one)
   -i: Only print indel variants
   -l: Limit the number of variants printed to this number
+  -F: Print the filename as the last column.
   -m: Print a different, tab-delimited format: the chromosome, the location,
       the top variant (major allele), its frequency, the #2 variant (minor
       allele), and its frequency (or more variants, if requested with -l):
@@ -33,6 +34,7 @@ my $counts = 0;
 my $majmin = 0;
 my $indels = 0;
 my $precise = 0;
+my $filename = 0;
 my $limit = $LIMIT_DEFAULT;
 my $last_was_limit = 0;
 my @new_args = ();
@@ -67,6 +69,9 @@ for my $arg (@ARGV) {
   }
   if ($arg =~ m/^-\w*i\w*$/) {
     $indels = 1; $opt = 1;
+  }
+  if ($arg =~ m/^-\w*F\w*$/) {
+    $filename = 1; $opt = 1;
   }
   if ($arg =~ m/^-\w*l\w*$/) {
     $last_was_limit = 1;
@@ -140,20 +145,19 @@ while (<>) {
         print ":".sprintf($format, "$percent")."%";
       }
     }
+    print "\t";
     $varcount++;
     if ($varcount >= $limit) {
       last;
     }
-    print "\t";
   }
   # always print a constant number of columns, even empty ones
-  if ($majmin && $varcount < $limit) {
-    print "\t";
+  while ($varcount < $limit) {
+    print "\t\t";
     $varcount++;
-    while ($varcount < $limit) {
-      print "\t\t";
-      $varcount++;
-    }
+  }
+  if ($filename) {
+    print "$ARGV\t";
   }
   print "\n";
 }
