112f9dbb0aef35b3f930a28293ea59f957c3c5f6
me <nmapsy@gmail.com>
2014-01-15 14:52:12 -0500

    cigar-naive.py renamed sam-filter.py, overhauled to be general filter, added clipping filter.

--- sam-filter.py
+++ sam-filter.py
@@ -3,18 +3,19 @@
 # Test data has shown an example of a deletion occurring beyond the border of
 # the chromosome. Just check that it's in a valid coordinate range.
 # - occurred in M01368:8:000000000-A3GHV:1:1108:18888:4511 (sample R19S5)
+from __future__ import division
 import re
 import os
 import sys
 from optparse import OptionParser
 
 USAGE = "USAGE: %prog [options] file.sam"
-DESCRIPTION = """A quick-and-dirty script to parse the CIGAR strings in a SAM
-file.
+DESCRIPTION = """Slightly more advanced SAM filtering than SAMTools. E.g. CIGAR
+parsing, NM tag, etc.
 Give "-" as the input filename to read from stdin."""
 EPILOG = """"""
-OPT_DEFAULTS = {'region':'', 'event_types':'ID', 'coord':0, 'sam_output':False,
-  'no_header':False, 'test_output':False}
+OPT_DEFAULTS = {'region':'', 'event_types':'', 'clipping':0.0, 'coord':0,
+  'sam_output':False, 'no_header':False, 'test_output':False}
 
 TESTS = [
   '251M',       # M
@@ -27,10 +28,8 @@ TESTS = [
 
 # try to set debug as a global
 debug = False
-if '-d' in sys.argv[1:] or '--debug' in sys.argv[1:]:
-  debug = True
 for opt in sys.argv[1:]:
-  if re.match(r'^-[a-zA-Z]*d', opt):
+  if opt == '--debug' or re.search(r'^-\w*d', opt):
     debug = True
 
 def main():
@@ -46,37 +45,43 @@ def main():
     help='Give a specific coordinate to restrict the indel search to.')
   parser.add_option('-e', '--event-types', dest='event_types',
     default=OPT_DEFAULTS.get('event_types'),
-    help=('The CIGAR operations to output: give a string of letters for each '
-      +'you want to include, e.g. "ID" for insertions and deletions (currently '
-      +'the only supported ones). Defaults: "%default"'))
-  parser.add_option('-s', '--sam-output', dest='sam_output',
-    action='store_const', const=not(OPT_DEFAULTS.get('sam_output')),
-    default=OPT_DEFAULTS.get('sam_output'),
-    help='When a read with a matching indel is found, print the original line '
-      +'from the SAM file instead of the indel data.')
+    help='Filter for CIGAR string events. Reads will only pass this filter if '
+      +'they have one of the given CIGAR events that *starts* at the '
+      +'coordinate given with -c (if no coordinate is given, any reads with '
+      +'the event anywhere will pass. Specify the event types by their CIGAR '
+      +'characters, e.g. "ID" for insertions and deletions (currently the only '
+      +'supported ones).')
+  parser.add_option('-C', '--clipping', dest='clipping', type='float',
+    default=OPT_DEFAULTS.get('clipping'),
+    help='Filter out reads which have more than this much clipping, as a '
+      +'percentage of their length.')
+  parser.add_option('-I', '--indel-output', dest='indel_output',
+    action='store_true', default=OPT_DEFAULTS.get('indel_output'),
+    help='Just print information on found indels passing the filters, instead '
+    +'of SAM output. This is a legacy output. If it is enabled, all filters '
+    +'except the CIGAR event types one will be ignored.')
   parser.add_option('-H', '--no-header', dest='no_header',
-    action='store_const', const=not(OPT_DEFAULTS.get('no_header')),
-    default=OPT_DEFAULTS.get('no_header'),
-    help='If using SAM output (-s), don\'t include the header.')
+    action='store_true', default=OPT_DEFAULTS.get('no_header'),
+    help='Don\'t include the SAM header.')
   parser.add_option('-T', '--test-output', dest='test_output',
-    action='store_const', const=not(OPT_DEFAULTS.get('test_output')),
-    default=OPT_DEFAULTS.get('test_output'),
+    action='store_true', default=OPT_DEFAULTS.get('test_output'),
     help='Generate original test output format on the data.')
   parser.add_option('-d', '--debug', dest='debug', action='store_true',
-    help=('Turn on debug mode.'))
+    help='Turn on debug mode.')
 
   (options, arguments) = parser.parse_args()
 
+  indel_output = options.indel_output
   test_output = options.test_output
   event_types = options.event_types
-  sam_output = options.sam_output
+  clipping = options.clipping
   no_header = options.no_header
   region = options.region
   coord = options.coord
 
   if not arguments:
     parser.print_help()
-    fail("Provide a filename (or \"-\") as an argument")
+    fail("\nProvide a filename (or \"-\") as an argument")
   else:
     infilename = arguments[0]
 
@@ -85,17 +90,22 @@ def main():
   else:
     infilehandle = open(infilename, 'r')
 
-  for line_orig in infilehandle:
+  for line in infilehandle:
+    passed = True
 
-    line = line_orig.strip()
+    line_stripped = line.strip()
+    # empty lines
     if not line:
       continue
-    if line[0] == '@' and line[3] == "\t":
-      if sam_output and not no_header:
-        sys.stdout.write(line_orig)
+    # header lines
+    if line_stripped[0] == '@' and line_stripped[3] == "\t":
+      if not (no_header or indel_output):
+        sys.stdout.write(line)
       continue
 
-    fields = line.split("\t")
+    fields = line_stripped.split("\t")
+    if len(fields) < 11:
+      fail("Format Error: Too few columns in SAM file. Line:\n"+line)
 
     name  = fields[0]
     cigar = fields[5]
@@ -103,17 +113,32 @@ def main():
     pos   = int(fields[3])
     if test_output:
       print "\n"+name+"\n"+str(pos)+": "+cigar
-    indels = parse_cigar(cigar, pos, seq=seq, test_output=test_output)
-    for indel in indels:
-      if indel[0] in event_types:
-        if not coord or coord == indel[1]:
-          if sam_output:
-            sys.stdout.write(line_orig)
-            break
-          else:
-            print indel
 
-  if infilehandle is not sys.stdin:
+    # CIGAR events filter
+    if event_types:
+      passed_filter = False
+      indels = parse_cigar(cigar, pos, seq=seq, test_output=test_output)
+      for indel in indels:
+        if indel[0] in event_types and (not coord or coord == indel[1]):
+          passed_filter = True
+          if indel_output:
+            print indel
+          else:
+            break
+      # if indel_output, skip rest of main loop to avoid printing SAM output
+      if passed_filter and indel_output:
+        continue
+      passed = passed and passed_filter
+
+    # clipping filter
+    if clipping:
+      passed_filter = clip_pass(cigar, clipping)
+      passed = passed and passed_filter
+
+    if passed:
+      sys.stdout.write(line)
+
+  if isinstance(infilehandle, file):
     infilehandle.close()
 
 
@@ -171,6 +196,45 @@ def parse_cigar(cigar, pos, seq='', test_output=False):
       pass
 
   return indels
+
+
+def clip_pass(cigar, threshold):
+  """Find whether the read passes the soft clipping threshold.
+  threshold is a maximum soft-clipping percentage (float)."""
+
+  clip_length = 0
+  total_length = 0
+  for move in re.findall(r'\d*[MIDNSHPX=]', cigar):
+    op = move[-1]
+    if len(move) > 1:
+      op_length = int(move[:-1])
+    else:
+      op_length = 1
+      sys.stderr.write("Warning: CIGAR op with no length (assumed 1): "+cigar)
+    if op == 'M':
+      total_length += op_length
+    elif op == 'I':
+      total_length += op_length
+    elif op == 'D':
+      pass
+    elif op == 'N':
+      pass
+    elif op == 'S':
+      total_length += op_length
+      clip_length += op_length
+    elif op == 'H':
+      total_length += op_length
+      clip_length += op_length
+    elif op == 'P':
+      pass
+
+  # print "clipped:%6.2f (%s)" % (100*clip_length/total_length,
+  #   ' '.join(re.findall(r'\d*[MIDNSHPX=]', cigar)))
+
+  if 100*clip_length/total_length > threshold:
+    return False
+  else:
+    return True
 
 
 def fail(message):
