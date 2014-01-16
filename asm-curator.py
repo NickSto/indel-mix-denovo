ab520ebe430acb17f78862b5727ba670050f24c1
me <nmapsy@gmail.com>
2014-01-16 05:24:03 -0500

    asm-curator.py: Added discarding wholly overlapping intervals.

--- asm-curator.py
+++ asm-curator.py
@@ -6,7 +6,7 @@ import quicksect
 import bioreaders
 from optparse import OptionParser
 
-OPT_DEFAULTS = {'str':'', 'int':0, 'float':0.0, 'bool':False}
+OPT_DEFAULTS = {'str':'', 'int':0, 'float':0.0, 'test_output':False}
 USAGE = "USAGE: %prog [options] lastz-output.lav assembly.fa"
 DESCRIPTION = """"""
 EPILOG = """"""
@@ -23,9 +23,9 @@ def main():
     default=OPT_DEFAULTS.get('int'), help='')
   parser.add_option('-f', '--float', dest='float', type='float',
     default=OPT_DEFAULTS.get('float'), help='')
-  parser.add_option('-b', '--bool', dest='bool', action='store_true',
-    default=OPT_DEFAULTS.get('bool'),
-    help='')
+  parser.add_option('-T', '--test-output', dest='test_output',
+    action='store_true', default=OPT_DEFAULTS.get('test_output'),
+    help='Print legacy test output.')
 
   (options, arguments) = parser.parse_args()
 
@@ -42,7 +42,16 @@ def main():
   lav.convert()
   intervals = lav_to_intervals(lav)
   all_overlaps = get_all_overlaps(intervals)
+  all_overlaps = discard_redundant(all_overlaps)
+  if options.test_output:
+    test_output(all_overlaps, intervals)
+    sys.exit()
+
+
+def test_output(all_overlaps, intervals):
   for interval in sorted(intervals, key=lambda x: x[0]):
+    if interval not in all_overlaps:
+      continue
     print "overlapping: ",format_interval(interval, intervals)
     for overlap in sorted(all_overlaps[interval], key=lambda x: x[0]):
       print format_interval(overlap, intervals)
@@ -60,6 +69,30 @@ def format_interval(interval, intervals):
   return output
 
 
+def discard_redundant(all_overlaps):
+  """Remove intervals wholly contained within other intervals.
+  The discarded intervals will be removed from both the set of keys and the
+  lists of overlapping intervals."""
+  redundant = set()
+  # gather redundant intervals
+  for interval in all_overlaps:
+    for overlap in all_overlaps[interval]:
+      if is_redundant(overlap, interval):
+        redundant.add(overlap)
+  # discard redundant intervals
+  for interval in all_overlaps.keys():
+    if interval in redundant:
+      del(all_overlaps[interval])
+    else:
+      all_overlaps[interval] = filter(lambda x: x not in redundant, all_overlaps[interval])
+  return all_overlaps
+
+
+def is_redundant(query, subject):
+  """Return True if query is wholly contained within subject."""
+  return subject[0] <= query[0] and query[1] <= subject[1]
+
+
 def lav_to_intervals(lav):
   """Convert a LASTZ alignment to a series of intervals along the reference
   sequence.
@@ -75,6 +108,9 @@ def lav_to_intervals(lav):
 
 
 def get_all_overlaps(intervals):
+  """Compare each interval to the rest, finding ones with any overlap.
+  Returns a dict mapping each interval to a list of intervals which overlap.
+  The original interval is always excluded from the list."""
   # build tree
   tree = None
   random.seed(1)
@@ -96,7 +132,7 @@ def get_all_overlaps(intervals):
   return all_overlaps
 
 
-def intlen(interval):
+def length(interval):
   return interval[1] - interval[0] + 1
 
 
