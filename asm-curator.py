dbca670a626962a90a54f1a45b016b333c69f268
me <nmapsy@gmail.com>
2014-01-22 12:13:47 -0500

    asm-curator.py: Added assembly analysis and report generation

--- asm-curator.py
+++ asm-curator.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
+# requires Python 2.7
+from __future__ import division
 import os
 import sys
 import random
 import quicksect
 import bioreaders
+import collections
 from optparse import OptionParser
 
-OPT_DEFAULTS = {'lav':'', 'asm':'', 'ref':'', 'contig_limit':1000, 'slop':20,
-  'test_output':False}
+OPT_DEFAULTS = {'lav':'', 'asm':'', 'ref':'', 'report':'', 'fragmented':500,
+  'minor_len':600, 'min_flank':75, 'min_gap':10, 'contig_limit':10000,
+  'slop':20, 'test_output':False}
 USAGE = ("USAGE: %prog [opts] "
   +"(-a asm.fa -r ref.fa|-l align.lav) [-o asm-new.fa]")
 DESCRIPTION = """Analyze an assembly via its LASTZ alignment to the reference,
@@ -15,11 +19,25 @@ and curate the assembly sequence. Provide either a LASTZ alignment (in LAV
 format) or the assembly and reference sequence so that an alignment can be
 performed. It will print to stdout an analysis of the alignment and any assembly
 issues it indicates. If a new assembly filename is provided with -o, it will
-create a curated assembly, with some redundant contigs removed.
+write a curated assembly there, with some redundant contigs removed. If LASTZ
+finds no hits, the curated assembly file will be created, but empty. On LAV
+input: this is only written to work with output of LASTZ 1.02.00 run with
+default options (only arguments are the reference and assembly filenames, then
+"--format=lav").
 Uses curation algorithm version 2: Just remove contigs which are entirely
 contained within another contig (in terms of their footprints on the reference).
 """
 EPILOG = """"""
+
+DUPLICATE_THRES = 1.8
+REPORT_TEXT = {
+  'hits':['h','contigs with hits to the reference'],
+  'curated':['c','contigs kept after curation'],
+  'failure':['F','assembly failure: no contigs with similarity to the reference'],
+  'fragmented':['f','fragmented assembly: a very high number of contigs'],
+  'duplicated':['d','whole-chromosome duplications'],
+  'nonref_flanks':['n','non-reference contig flanks'],
+}
 
 #TODO: Distinguish contigs with exactly the same start/end points.
 #      Maybe use IntervalNode.linenum as unique identifier.
@@ -40,13 +58,33 @@ def main():
     help='The reference genome, in a FASTA file.')
   parser.add_option('-o', '--out', dest='out',
     default=OPT_DEFAULTS.get('out'),
-    help='Write a curated version of the assembly to this filename.')
+    help='Write a curated version of the assembly to this file.')
+  parser.add_option('-R', '--report', dest='report',
+    default=OPT_DEFAULTS.get('report'),
+    help='Write an analysis of the assembly to this file instead of stdout.')
+  parser.add_option('-f', '--fragmented', dest='fragmented', type='int',
+    default=OPT_DEFAULTS.get('fragmented'),
+    help="""Assemblies with more than this number of contigs will be marked as
+"fragmented" in the report. Currently the contig number is approximated by the
+number of LASTZ hits. Set to 0 to never mark as fragmented. Default: %default""")
+  parser.add_option('-m', '--minor-len', dest='minor_len', type='int',
+    default=OPT_DEFAULTS.get('minor_len'),
+    help="""Minor contig threshold. Contigs longer than this will be considered
+"major" contigs, and the rest are "minor" contigs. Default: %default""")
+  parser.add_option('-F', '--min-flank', dest='min_flank', type='int',
+    default=OPT_DEFAULTS.get('min_flank'),
+    help="""Minimum length a non-reference flank must be to count it in the
+report. Only major contigs are considered. Default: %default""")
+  parser.add_option('-g', '--min-gap', dest='min_gap', type='int',
+    default=OPT_DEFAULTS.get('min_gap'),
+    help="""Minimum length for an assembly gap to be counted in the report.
+Default: %default""")
   parser.add_option('-C', '--contig-limit', dest='contig_limit', type='int',
     default=OPT_DEFAULTS.get('contig_limit'),
     help="""Maximum allowed contigs in the assembly. If there are more than this
 many contigs in the assembly, abort to avoid exceeding resources. Currently the
 number of contigs is approximated by the number of LASTZ hits. Set to 0 for no
-limit (at your own risk). Default: %default""")
+limit. Default: %default""")
   parser.add_option('-s', '--slop', dest='slop', type='int',
     default=OPT_DEFAULTS.get('slop'),
     help="""Leeway in the contig overlap comparison. Essentially, when deciding
@@ -59,6 +97,7 @@ overlap situation, only the smaller contig is removed. Default: %default""")
 
   (options, arguments) = parser.parse_args()
 
+  # set up I/O
   files = []
   if options.lav:
     files.append(options.lav)
@@ -77,7 +116,10 @@ overlap situation, only the smaller contig is removed. Default: %default""")
       parser.print_help()
       fail("\nError: Please provide the assembly FASTA file in order to create "
         +"a curated assembly.")
-
+  if options.report:
+    report_handle = open(options.report, 'w')
+  else:
+    report_handle = sys.stdout
   for path in files:
     if not os.path.isfile(path):
       fail("\nError: cannot find input file "+path)
@@ -107,11 +149,14 @@ overlap situation, only the smaller contig is removed. Default: %default""")
     if interval not in all_overlaps:
       del(intervals[interval])
 
-  #TODO: print alignment analysis
+  report = get_report(lav, intervals, options)
+  report_handle.write(format_report(report, REPORT_TEXT))
+
+  if isinstance(report_handle, file):
+    report_handle.close()
 
   if not (options.asm and options.out):
     sys.exit(0)
-
   curate(intervals, options.asm, options.out)
 
 
@@ -120,35 +165,8 @@ def length(interval):
   return interval[1] - interval[0] + 1
 
 
-def test_output(all_overlaps, intervals):
-  for interval in sorted(intervals, key=lambda x: x[0]):
-    if interval not in all_overlaps:
-      continue
-    print "overlapping",format_interval(interval, intervals)
-    # for overlap in sorted(all_overlaps[interval], key=lambda x: x[0]):
-    #   print format_interval(overlap, intervals)
-    # print "  unique:"
-    # merged = merge(all_overlaps[interval])
-    # merged.sort(key=lambda x: x[0])
-    # for unique in get_uniques(merged, interval):
-    #   print "    "+format_interval(unique)
-
-
 def align(asmpath, refpath):
   fail("LASTZ ALIGNMENT NOT YET IMPLEMENTED")
-
-
-def format_interval(interval, intervals=None):
-  output = str(interval)
-  output += ' '+str(interval[1]-interval[0]+1)+' bp'
-  if intervals:
-    name = intervals[interval].parent.query['id']
-    if name.startswith('NODE_'):
-      fields = name.split('_')
-      if len(fields) > 2:
-        name = '_'.join(fields[:2])
-    output += ":\t"+name
-  return output
 
 
 def lav_to_intervals(lav):
@@ -211,21 +229,66 @@ def discard_redundant(all_overlaps, slop=0):
 
 def is_redundant(interval, overlaps, slop=0):
   """Return True if the interval is redundant."""
-  # print "is_redundant on "+format_interval(interval)
   for overlap in overlaps:
     # if interval is wholly contained within overlap (+ slop)
     if (overlap[0] - slop <= interval[0] and interval[1] <= overlap[1] + slop
         and length(interval) < length(overlap)):
-      # print "  wholly contained within "+format_interval(overlap)
       return True
   return False
 
 
-def merge(intervals):
+def get_report(lav, intervals, options):
+  """Generate report on the assembly quality.
+  Call after redundants have been removed from intervals.
+  Assumption: The entire query and subject sequences have been used in alignment
+  (the end coordinate in the LAV h stanza == the sequence length)."""
+  report = collections.OrderedDict()
+  # number of "contigs"
+  report['hits'] = len(lav)
+  # number of kept contigs
+  report['curated'] = len(get_contig_names(intervals))
+  # failed assembly? (zero good contigs)
+  if len(lav) == 0:
+    report['failure'] = True
+    return report
+  else:
+    report['failure'] = False
+  # failed assembly? (many contigs)
+  if options.fragmented and len(lav) > options.fragmented:
+    report['fragmented'] = True
+  else:
+    report['fragmented'] = False
+  # is there a contig that looks like a double of the reference?
+  report['duplicated'] = 0
+  for hit in lav:
+    # length of contig over length of reference
+    if hit.query['end'] / hit.subject['end'] > DUPLICATE_THRES:
+      #TODO: check that there are actually two alignments ~ as big as reference
+      report['duplicated'] += 1
+  # are there contigs with long flanks of non-reference sequence?
+  report['nonref_flanks'] = len(get_nonref_flanks(intervals, options))
+  # how many gaps are in the assembly?
+  #TODO: take chromosome into account
+  merged = merge(intervals.keys(), sort=True)
+  gaps = subtract(merged, (1, lav.hits[0].subject['end']))
+  report['gaps'] = len(gaps)
+  return report
+
+
+def format_report(report, report_text):
+  always_print = ['hits', 'curated']
+  output = ''
+  for issue in report:
+    if report[issue] or issue in always_print:
+      messages = report_text[issue]
+      output += "\t".join([messages[0], str(report[issue]), messages[1]]) + "\n"
+  return output
+
+
+def merge(intervals, sort=False):
   """Merge a list of intervals into a new list of non-overlapping intervals.
-  Merging is 1-based (will merge if end == start).
-  Implementation is totally naive: O(n^2)."""
-  #TODO: replace with an actually efficient merge, e.g. from bx-python
+  Optionally sort the output list by starting coordinate. Merging is 1-based
+  (will merge if end == start). Totally naive implementation: O(n^2)."""
   merged = []
   # go through input list, adding 1 interval at a time to a growing merged list
   for interval in intervals:
@@ -241,33 +304,74 @@ def merge(intervals):
       else:
         i+=1
     merged.append(interval)
+  if sort:
+    merged.sort(key=lambda x: x[0])
   return merged
 
 
-def get_uniques(merged, int_of_interest):
-  """Find all regions in overlaps that do not overlap with any other interval.
-  merged is a merge of all the intervals overlapping the one of interest. It
-  must be a set of non-overlapping intervals, in order from lowest to highest
-  coordinate.
-  interval is the interval of interest.
-  Returns a list of intervals representing the unique spans."""
-  begin = int_of_interest[0]
-  end = int_of_interest[1]
-  uniques = []
+def subtract(merged, interval):
+  """Subtract intervals in merged from interval. Merged must be a list of
+  "merged" intervals: non-overlapping, in order from lowest to highest
+  coordinate. Returns a list of intervals representing the regions of interval
+  not overlapping any intervals in merged."""
+  begin = interval[0]
+  end = interval[1]
+  results = []
   for i in range(len(merged)):
-    # add region before first overlap (if unique)
+    # add region before first overlap (if any)
     if i == 0 and begin < merged[i][0]:
-      uniques.append((begin, merged[i][0]-1))
+      results.append((begin, merged[i][0]-1))
     if i > 0:
-      unique = (merged[i-1][1]+1, merged[i][0]-1)
-      if unique[0] <= unique[1]:
-        uniques.append(unique)
-    # add region after last overlap (if unique)
+      result = (merged[i-1][1]+1, merged[i][0]-1)
+      if result[0] <= result[1]:
+        results.append(result)
+    # add region after last overlap (if any)
     if i == len(merged) - 1 and end > merged[i][1]:
-      uniques.append((merged[i][1]+1, end))
+      results.append((merged[i][1]+1, end))
   if not merged:
-    uniques.append((begin, end))
-  return uniques
+    results.append((begin, end))
+  return results
+
+
+def get_nonref_flanks(intervals, options):
+  """Return a list of the names of contigs which have non-reference flanking
+  sequences. There will be one entry for every flank, so a contig with two will
+  appear twice (and len() of the list == number of flanks)."""
+  # build a map of contigs to their regions that align to the reference
+  asm_ref_regions = {}
+  asm_lengths = {}
+  for interval in intervals:
+    hit = intervals[interval].parent
+    queryname = hit.query['name']
+    if queryname in asm_ref_regions:
+      continue
+    asm_lengths[queryname] = hit.query['end']
+    ref_regions = []
+    for alignment in hit:
+      interval = (alignment.query['begin'], alignment.query['end'])
+      ref_regions.append(interval)
+    asm_ref_regions[queryname] = merge(ref_regions, sort=True)
+  # check contigs for non-reference flanks
+  nonref_flanks = []
+  for queryname in asm_ref_regions:
+    length = asm_lengths[queryname]
+    ref_regions = asm_ref_regions[queryname]
+    if length <= options.minor_len:
+      continue
+    # a contig with no alignment shouldn't appear, but deal with it anyway
+    # by counting it as having two nonref flanks
+    if not ref_regions:
+      if length/2 >= options.min_flank:
+        nonref_flanks.append(queryname)
+        nonref_flanks.append(queryname)
+      continue
+    lflank = ref_regions[0][0] - 1
+    rflank = length - ref_regions[-1][1]
+    if lflank > options.min_flank:
+      nonref_flanks.append(queryname)
+    if rflank > options.min_flank:
+      nonref_flanks.append(queryname)
+  return nonref_flanks
 
 
 def curate(intervals, asmpath, outpath):
@@ -297,6 +401,33 @@ def get_contig_names(intervals):
     hit = alignment.parent
     names.add(hit.query['name'])
   return names
+
+
+def test_output(all_overlaps, intervals):
+  for interval in sorted(intervals, key=lambda x: x[0]):
+    if interval not in all_overlaps:
+      continue
+    print "overlapping",format_interval(interval, intervals)
+    # for overlap in sorted(all_overlaps[interval], key=lambda x: x[0]):
+    #   print format_interval(overlap, intervals)
+    # print "  unique:"
+    # merged = merge(all_overlaps[interval])
+    # merged.sort(key=lambda x: x[0])
+    # for unique in get_uniques(merged, interval):
+    #   print "    "+format_interval(unique)
+
+
+def format_interval(interval, intervals=None):
+  output = str(interval)
+  output += ' '+str(interval[1]-interval[0]+1)+' bp'
+  if intervals:
+    name = intervals[interval].parent.query['id']
+    if name.startswith('NODE_'):
+      fields = name.split('_')
+      if len(fields) > 2:
+        name = '_'.join(fields[:2])
+    output += ":\t"+name
+  return output
 
 
 def fail(message):
