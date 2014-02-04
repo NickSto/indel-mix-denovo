cb2e3c333105102f3c3f2bf9d598c76965567951
Nick <nmapsy@gmail.com>
2014-02-04 04:13:04 -0500

    inspect-reads.py: use simplewrap.py to wrap help text to any terminal width. - also, migrate from bioreaders to vcfreader.

--- inspect-reads.py
+++ inspect-reads.py
@@ -4,7 +4,8 @@ bamslicer module, so that it can also be used by nvc-filter.py. Then
 nvc-filter.py can use the statistics as a filter."""
 from __future__ import division
 import collections
-import bioreaders
+import simplewrap
+import vcfreader
 import bamslicer
 import argparse
 import sys
@@ -12,22 +13,30 @@ import os
 # ./inspect-reads.py ~/backuphide/bfx/R19S5-new-nm.bam -H -v chrM-R19S5-new-nm-dedup:15873-I,chrM-R19S5-new-nm-dedup:15873-D,chrM-R19S5-new-nm-dedup:13718-D,chrM-R19S5-new-nm-dedup:11571-D,chrM-R19S5-new-nm-dedup:3757-D
 # ./inspect-reads.py tests/cigar-tests.bam -H -v chrM:5-I,chrM:199-D,chrM:199-I,chrM:3106-D,chrM:6110-D,chrM:16568-I
 
+"""Need to use argparse.RawTextHelpFormatter to preserve formatting in the
+description of columns in the tsv output. But to still accommodate different
+terminal widths, dynamic wrapping with simplewrap will be necessary."""
+WIDTH = simplewrap.termwidth(70)
+
 OPT_DEFAULTS = {'human':True, 'tsv':False, 'vartypes':'ID',
   'strand_bias':0, 'mate_bias':0}
 USAGE = "%(prog)s [options] -v variantslist -V variants.vcf reads.bam"
-DESCRIPTION = ("Retrieve the reads supporting a given set of variants, and "
-+"report statistics on"+"""
-them. Provide the variants in a VCF and/or in a list on the command line.""")
-EPILOG = """WARNING: Work in progress. Not yet implemented features:
---containing --output-bam --all --opposing options
-Considering the "alt" information of variants (the only thing considered right
-now is the location and type).
-And most important of all, ***SNVS ARE NOT SUPPORTED!*** Right now only indels
-are recognized."""
+DESCRIPTION = simplewrap.wrap("Retrieve the reads supporting a given set of "
+  "variants, and report statistics on them. Provide the variants in a VCF "
+  "and/or in a list on the command line.", width=WIDTH)
+EPILOG = simplewrap.wrap("WARNING: Work in progress. Not yet implemented "
+  "features:\n"
+  "--containing --output-bam --all --opposing options\n"
+  "Considering the \"alt\" information of variants (the only thing considered "
+  "right now is the location and type).\n"
+  "And most important of all, ***SNVS ARE NOT SUPPORTED!*** Right now only "
+  "indels are recognized.", width=WIDTH)
 
 VALID_BASES = 'GATCNgatcn'
 
 def main():
+
+  wrap = simplewrap.wrapper(width=WIDTH-24)
 
   parser = argparse.ArgumentParser(usage=USAGE, description=DESCRIPTION,
     epilog=EPILOG, formatter_class=argparse.RawTextHelpFormatter)
@@ -36,16 +45,14 @@ def main():
   parser.add_argument('bamfilepath', metavar='reads.bam',
     help="""the input reads""")
   parser.add_argument('-v', '--variants',
-    help="""Use these variants. Give a comma-separated list, in the
-format "chrom:pos-type[:alt]" e.g. "chr1:2345-D:2".
-"pos" is the 1-based coordinate of the SNV, or the base
-before the insertion or deletion. "type" is "S", "I", or
-"D", for SNV, insertion, or deletion. "alt" is optional,
-and specifies the alternate allele: the SNV alternate
-base, the length of the deletion, or the inserted
-sequence (including the preceding base, as in VCF). If
-"alt" is not provided, it will select any read with that
-type of variant at that location.""")
+    help=wrap('Use these variants. Give a comma-separated list, in the format '
+      '"chrom:pos-type[:alt]" e.g. "chr1:2345-D:2". "pos" is the 1-based '
+      'coordinate of the SNV, or the base before the insertion or deletion. '
+      '"type" is "S", "I", or "D", for SNV, insertion, or deletion. "alt" is '
+      'optional, and specifies the alternate allele: the SNV alternate base, '
+      'the length of the deletion, or the inserted sequence (including the '
+      'preceding base, as in VCF). If "alt" is not provided, it will select '
+      'any read with that type of variant at that location.'))
   parser.add_argument('-V', '--vcf',
     help='Use the variants in the ALT column of this VCF file.')
   parser.add_argument('-o', '--output-bam',
@@ -53,67 +60,63 @@ type of variant at that location.""")
   parser.add_argument('-H', '--human', action='store_true',
     help="""Print statistics in a human-readable format (default).""")
   parser.add_argument('-t', '--tsv', action='store_true',
-    help="""Print statistics in a tab-delimited columnar format. The
-first four columns are the same data as described in the
---variants format. The strand and mate bias statistics
-are described in the -s and -m options. The null value
-is "None".
-Columns:
-  1:  chrom
-  2:  coord
-  3:  variant type
-  4:  alt allele
-  5:  coverage, in # of reads
-  6:  # of variant-supporting reads
-  7:  %% frequency
-  8:  %% of supporting reads that are unmapped
-  9:  %% not mapped in proper pair
-  10: %% on the forward strand
-  11: %% that are the 1st mate in the pair
-  12: %% that are a secondary alignment
-  13: %% that are marked duplicates
-  14: %% with a MAPQ == 0
-  15: %% with a MAPQ >= 20
-  16: %% with a MAPQ >= 30
-  17: %% with the highest MAPQ
-  18: the highest MAPQ in the supporting reads
-  19: strand bias
-  20: mate bias
-  21: The total number of supporting reads with each SAM
-      flag. This is a comma-separated list of the total
-      for each flag, from lowest to highest bit value.""")
+    help=wrap('Print statistics in a tab-delimited columnar format. The first '
+      'four columns are the same data as described in the --variants format. '
+      'The strand and mate bias statistics are described in the -s and -m '
+      'options. The null value is "None".\n'
+      'Columns:')+'\n'+simplewrap.wrap(
+      '1:  chrom\n'
+      '2:  coord\n'
+      '3:  variant type\n'
+      '4:  alt allele\n'
+      '5:  coverage, in # of reads\n'
+      '6:  # of variant-supporting reads\n'
+      '7:  %% frequency\n'
+      '8:  %% of supporting reads that are unmapped\n'
+      '9:  %% not mapped in proper pair\n'
+      '10: %% on the forward strand\n'
+      '11: %% that are the 1st mate in the pair\n'
+      '12: %% that are a secondary alignment\n'
+      '13: %% that are marked duplicates\n'
+      '14: %% with a MAPQ == 0\n'
+      '15: %% with a MAPQ >= 20\n'
+      '16: %% with a MAPQ >= 30\n'
+      '17: %% with the highest MAPQ\n'
+      '18: the highest MAPQ in the supporting reads\n'
+      '19: strand bias\n'
+      '20: mate bias'
+      '21: The total number of supporting reads with each SAM flag. This is a '
+          'comma-separated list of the total for each flag, from lowest to '
+          'highest bit value.',
+      width=WIDTH-24, subsequent_indent='    '))
   parser.add_argument('-L', '--no-labels', action='store_true',
-    help="""If csv output is selected, do not print column labels
-(normally the first line, begins with #).""")
+    help=wrap('If csv output is selected, do not print column labels (normally '
+      'the first line, begins with #).'))
   parser.add_argument('-T', '--vartypes', metavar='TYPES',
-    help="""Only consider these variant types. Give a string of
-letters, e.g. "SID" to keep SNVs (S), insertions (I),
-and deletions (D). Default: "%(default)s" """)
+    help=wrap('Only consider these variant types. Give a string of letters, '
+      'e.g. "SID" to keep SNVs (S), insertions (I), and deletions (D). '
+      'Default: "%(default)s"'))
   parser.add_argument('-a', '--all', action='store_true',
-    help="""Select all the reads covering the variants, not just
-those supporting the variant. The statistics reported
-will be on these reads, not the supporting reads.""")
+    help=wrap('Select all the reads covering the variants, not just those '
+      'supporting the variant. The statistics reported will be on these reads, '
+      'not the supporting reads.'))
   parser.add_argument('-O', '--opposing', action='store_true',
     default=OPT_DEFAULTS.get('opposing'),
-    help="""Select the reads OPPOSING the variants. The statistics
-reported will be on these reads, not the supporting
-reads.""")
+    help=wrap('Select the reads OPPOSING the variants. The statistics reported '
+      'will be on these reads, not the supporting reads.'))
   parser.add_argument('-C', '--containing', action='store_true',
-    help="""For deletions, select any read with a deletion which
-*contains* the specified one. So if chr1:2345-D:2 is a
-variant of interest, and a read has the variant
-chr1:2344-D:3, it will select that read even though the
-variants don't have the same start coordinates.""")
+    help=wrap('For deletions, select any read with a deletion which *contains* '
+      'the specified one. So if chr1:2345-D:2 is a variant of interest, and a '
+      'read has the variant chr1:2344-D:3, it will select that read even '
+      'though the variants don\'t have the same start coordinates.'))
   parser.add_argument('-s', '--strand-bias', type=float, metavar='SBIAS',
-    help="""Filter out variants with a strand bias value greater
-than or equal to this. The strand bias statistic is
-method 1 (SB) of Guo et al., 2012. A typical cutoff is
-1.0.""")
+    help=wrap('Filter out variants with a strand bias value greater than or '
+      'equal to this. The strand bias statistic is method 1 (SB) of Guo et '
+      'al., 2012. A typical cutoff is 1.0.'))
   parser.add_argument('-m', '--mate-bias', type=float, metavar='MBIAS',
-    help="""Filter out variants with a higher mate bias value than
-this. The statistic is calculated identically to the
-strand bias one, but replacing forward/reverse strand
-with first/second mate in the pair.""")
+    help=wrap('Filter out variants with a higher mate bias value than this. '
+      'The statistic is calculated identically to the strand bias one, but '
+      'replacing forward/reverse strand with first/second mate in the pair.'))
 
   args = parser.parse_args()
 
@@ -214,8 +217,8 @@ def valid_variant(vartype, alt):
 def variants_from_vcf(vcffilename):
   #TODO: support multiple samples
   variants = []
-  vcfreader = bioreaders.VCFReader(open(vcffilename, 'rU'))
-  for site in vcfreader:
+  vcf = vcfreader.VCFReader(open(vcffilename, 'rU'))
+  for site in vcf:
     chrom = site.get_chrom()
     pos = site.get_pos()
     ref = site.get_ref()
