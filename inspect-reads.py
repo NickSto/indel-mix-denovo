f6a45f51616dd5124e1f1f030d5921ae8a744ca8
Nick <nmapsy@gmail.com>
2014-02-04 01:52:07 -0500

    inspect-reads.py: Switched from optparse to argparse

--- inspect-reads.py
+++ inspect-reads.py
@@ -6,120 +6,130 @@ from __future__ import division
 import collections
 import bioreaders
 import bamslicer
-import optparse
+import argparse
 import sys
 import os
 # ./inspect-reads.py ~/backuphide/bfx/R19S5-new-nm.bam -H -v chrM-R19S5-new-nm-dedup:15873-I,chrM-R19S5-new-nm-dedup:15873-D,chrM-R19S5-new-nm-dedup:13718-D,chrM-R19S5-new-nm-dedup:11571-D,chrM-R19S5-new-nm-dedup:3757-D
 # ./inspect-reads.py tests/cigar-tests.bam -H -v chrM:5-I,chrM:199-D,chrM:199-I,chrM:3106-D,chrM:6110-D,chrM:16568-I
 
-OPT_DEFAULTS = {'variants':'', 'vcf':'', 'output_bam':'', 'human':True,
-  'tsv':False, 'no_labels':False, 'vartypes':'ID', 'all':False, 'opposing':False,
-  'containing':False, 'strand_bias':0, 'mate_bias':0}
-USAGE = "USAGE: %prog [options] (-v variantslist|-V variants.vcf) reads.bam"
-DESCRIPTION = """Retrieve the reads supporting a given set of variants, and
-report statistics on them. Provide the variants in a VCF or in a list on the
-command line."""
+OPT_DEFAULTS = {'human':True, 'tsv':False, 'vartypes':'ID',
+  'strand_bias':0, 'mate_bias':0}
+USAGE = "%(prog)s [options] -v variantslist -V variants.vcf reads.bam"
+DESCRIPTION = ("Retrieve the reads supporting a given set of variants, and "
++"report statistics on"+"""
+them. Provide the variants in a VCF and/or in a list on the command line.""")
 EPILOG = """WARNING: Work in progress. Not yet implemented features:
---containing, --output-bam, --all, and --opposing options, and
-considering the "alt" information of variants (the only thing considered right
-now is the location and type). And most important of all, ***SNVS ARE NOT
-SUPPORTED!*** Right now only indels are recognized."""
+--containing --output-bam --all --opposing options
+Considering the "alt" information of variants (the only thing considered right
+now is the location and type).
+And most important of all, ***SNVS ARE NOT SUPPORTED!*** Right now only indels
+are recognized."""
 
 VALID_BASES = 'GATCNgatcn'
 
 def main():
 
-  parser = optparse.OptionParser(usage=USAGE, description=DESCRIPTION, epilog=EPILOG)
+  parser = argparse.ArgumentParser(usage=USAGE, description=DESCRIPTION,
+    epilog=EPILOG, formatter_class=argparse.RawTextHelpFormatter)
+  parser.set_defaults(**OPT_DEFAULTS)
 
-  parser.add_option('-v', '--variants', dest='variants',
-    default=OPT_DEFAULTS.get('variants'),
-    help="""Use these variants. Give a comma-separated list, in the format
-"chrom:pos-type[:alt]" e.g. "chr1:2345-D:2". "pos" is the 1-based coordinate
-of the SNV, or the base before the insertion or deletion. "type" is "S", "I", or
-"D", for SNV, insertion, or deletion. "alt" is optional, and specifies the
-alternate allele: the SNV alternate base, the length of the deletion, or the
-inserted sequence (including the preceding base, as in VCF). If "alt" is not
-provided, it will select any read with that type of variant at that location.""")
-  parser.add_option('-V', '--vcf', dest='vcf',
-    default=OPT_DEFAULTS.get('vcf'),
+  parser.add_argument('bamfilepath', metavar='reads.bam',
+    help="""the input reads""")
+  parser.add_argument('-v', '--variants',
+    help="""Use these variants. Give a comma-separated list, in the
+format "chrom:pos-type[:alt]" e.g. "chr1:2345-D:2".
+"pos" is the 1-based coordinate of the SNV, or the base
+before the insertion or deletion. "type" is "S", "I", or
+"D", for SNV, insertion, or deletion. "alt" is optional,
+and specifies the alternate allele: the SNV alternate
+base, the length of the deletion, or the inserted
+sequence (including the preceding base, as in VCF). If
+"alt" is not provided, it will select any read with that
+type of variant at that location.""")
+  parser.add_argument('-V', '--vcf',
     help='Use the variants in the ALT column of this VCF file.')
-  parser.add_option('-o', '--output-bam', dest='output_bam',
-    default=OPT_DEFAULTS.get('output_bam'),
+  parser.add_argument('-o', '--output-bam',
     help='Output the selected reads to this BAM file.')
-  parser.add_option('-H', '--human', dest='human', action='store_true',
-    default=OPT_DEFAULTS.get('human'),
-    help="""Print statistics in a human-readable format (default output).""")
-  parser.add_option('-t', '--tsv', dest='tsv', action='store_true',
-    default=OPT_DEFAULTS.get('tsv'),
-    help="""Print statistics in a tab-delimited columnar format. The first four
-columns are the same data as described in the --variants format. The strand and
-mate bias statistics are described in the -s and -m options. The null value is
-"None".
-Columns: (1) chrom (2) coord (3) variant type (4) alt allele (5) coverage, in #
-of reads (6) # of variant-supporting reads (7) % frequency (8) % of supporting
-reads that are unmapped (9) % not mapped in proper pair (10) % on the forward
-strand (11) % that are the 1st mate in the pair (12) % that are a secondary
-alignment (13) % that are marked duplicates (14) % with a MAPQ == 0 (15) % with
-a MAPQ >= 20 (16) % with a MAPQ >= 30 (17) % with the highest MAPQ (18) the
-highest MAPQ in the supporting reads (19) strand bias (20) mate bias (21) the
-total number of supporting reads with each SAM flag. This is a comma-separated
-list of the total for each flag, from lowest to highest bit value.""")
-  parser.add_option('-L', '--no-labels', dest='no_labels', action='store_true',
-    default=OPT_DEFAULTS.get('no_labels'),
-    help="""If csv output is selected, do not print column labels (normally the
-first line, begins with #).""")
-  parser.add_option('-T', '--vartypes', dest='vartypes',
-    default=OPT_DEFAULTS.get('vartypes'), help="""Only consider these variant
-types. Give a string of letters, e.g. "SID" to keep SNVs (S), insertions (I),
-and deletions (D). Default: "%default" """)
-  parser.add_option('-a', '--all', dest='all', action='store_true',
-    default=OPT_DEFAULTS.get('all'),
-    help="""Select all the reads covering the variants, not just those
-supporting the variant. The statistics reported will be on these reads, not the
-supporting reads.""")
-  parser.add_option('-O', '--opposing', dest='opposing', action='store_true',
+  parser.add_argument('-H', '--human', action='store_true',
+    help="""Print statistics in a human-readable format (default).""")
+  parser.add_argument('-t', '--tsv', action='store_true',
+    help="""Print statistics in a tab-delimited columnar format. The
+first four columns are the same data as described in the
+--variants format. The strand and mate bias statistics
+are described in the -s and -m options. The null value
+is "None".
+Columns:
+  1:  chrom
+  2:  coord
+  3:  variant type
+  4:  alt allele
+  5:  coverage, in # of reads
+  6:  # of variant-supporting reads
+  7:  %% frequency
+  8:  %% of supporting reads that are unmapped
+  9:  %% not mapped in proper pair
+  10: %% on the forward strand
+  11: %% that are the 1st mate in the pair
+  12: %% that are a secondary alignment
+  13: %% that are marked duplicates
+  14: %% with a MAPQ == 0
+  15: %% with a MAPQ >= 20
+  16: %% with a MAPQ >= 30
+  17: %% with the highest MAPQ
+  18: the highest MAPQ in the supporting reads
+  19: strand bias
+  20: mate bias
+  21: The total number of supporting reads with each SAM
+      flag. This is a comma-separated list of the total
+      for each flag, from lowest to highest bit value.""")
+  parser.add_argument('-L', '--no-labels', action='store_true',
+    help="""If csv output is selected, do not print column labels
+(normally the first line, begins with #).""")
+  parser.add_argument('-T', '--vartypes', metavar='TYPES',
+    help="""Only consider these variant types. Give a string of
+letters, e.g. "SID" to keep SNVs (S), insertions (I),
+and deletions (D). Default: "%(default)s" """)
+  parser.add_argument('-a', '--all', action='store_true',
+    help="""Select all the reads covering the variants, not just
+those supporting the variant. The statistics reported
+will be on these reads, not the supporting reads.""")
+  parser.add_argument('-O', '--opposing', action='store_true',
     default=OPT_DEFAULTS.get('opposing'),
-    help="""Select the reads OPPOSING the variants. The statistics reported will
-be on these reads, not the supporting reads.""")
-  parser.add_option('-C', '--containing', dest='containing',
-    action='store_true', default=OPT_DEFAULTS.get('containing'),
-    help="""For deletions, select any read with a deletion which *contains* the
-specified one. So if chr1:2345-D:2 is a variant of interest, and a read has the
-variant chr1:2344-D:3, it will select that read even though the variants don't
-have the same start coordinates.""")
-  parser.add_option('-s', '--strand-bias', dest='strand_bias', type='float',
-    default=OPT_DEFAULTS.get('strand_bias'),
-    help="""Filter out variants with a strand bias value greater than or equal
-to this. The strand bias statistic is method 1 (SB) of Guo et al., 2012. A
-typical cutoff is 1.0.""")
-  parser.add_option('-m', '--mate-bias', dest='mate_bias', type='float',
-    default=OPT_DEFAULTS.get('mate_bias'),
-    help="""Filter out variants with a higher mate bias value than this. The
-statistic is calculated identically to the strand bias one, but replacing
-forward/reverse strand with first/second mate in the pair.""")
+    help="""Select the reads OPPOSING the variants. The statistics
+reported will be on these reads, not the supporting
+reads.""")
+  parser.add_argument('-C', '--containing', action='store_true',
+    help="""For deletions, select any read with a deletion which
+*contains* the specified one. So if chr1:2345-D:2 is a
+variant of interest, and a read has the variant
+chr1:2344-D:3, it will select that read even though the
+variants don't have the same start coordinates.""")
+  parser.add_argument('-s', '--strand-bias', type=float, metavar='SBIAS',
+    help="""Filter out variants with a strand bias value greater
+than or equal to this. The strand bias statistic is
+method 1 (SB) of Guo et al., 2012. A typical cutoff is
+1.0.""")
+  parser.add_argument('-m', '--mate-bias', type=float, metavar='MBIAS',
+    help="""Filter out variants with a higher mate bias value than
+this. The statistic is calculated identically to the
+strand bias one, but replacing forward/reverse strand
+with first/second mate in the pair.""")
 
-  (options, arguments) = parser.parse_args()
-
-  if arguments:
-    #TODO: support multiple BAMs
-    bamfilepath = arguments[0]
-  else:
-    parser.print_help()
-    fail("\nError: Please provide a BAM file and a list of variants.")
+  args = parser.parse_args()
 
   variants = []
-  if options.vcf:
-    variants.extend(variants_from_vcf(options.vcf))
-  if options.variants:
-    variants.extend(variants_from_str(options.variants))
+  if args.vcf:
+    variants.extend(variants_from_vcf(args.vcf))
+  if args.variants:
+    variants.extend(variants_from_str(args.variants))
   if not variants:
     parser.print_help()
     fail("\nError: Please provide a list of variants in either a VCF file or a "
       +"command line option.")
   # eliminate variants that aren't one of the specified types
-  variants[:] = [var for var in variants if var['type'] in options.vartypes]
+  variants[:] = [var for var in variants if var['type'] in args.vartypes]
 
-  if options.tsv:
+  if args.tsv:
     outformat = 'tsv'
   else:
     outformat = 'human'
@@ -129,10 +139,11 @@ forward/reverse strand with first/second mate in the pair.""")
   # sort variants by coordinate
   variants.sort(key=lambda variant: variant['coord'])
 
-  (read_sets, stat_sets) = bamslicer.get_reads_and_stats(bamfilepath, variants)
+  (read_sets, stat_sets) = bamslicer.get_reads_and_stats(
+    args.bamfilepath, variants)
 
   for (variant, reads, stats) in zip(variants, read_sets, stat_sets):
-    if filter_out(variant, reads, stats, options):
+    if filter_out(variant, reads, stats, args):
       continue
     output_stats = get_output_stats(variant, stats)
     if outformat == 'human':
@@ -238,14 +249,14 @@ def parse_varstr(varstr, chrom, coord):
     return {'chrom':chrom, 'coord':coord, 'type':'I', 'alt':varstr}
 
 
-def filter_out(variant, reads, stats, options):
+def filter_out(variant, reads, stats, args):
   filter_out = False
-  if options.strand_bias and stats['strand_bias'] is not None:
-    if stats['strand_bias'] > options.strand_bias:
+  if args.strand_bias and stats['strand_bias'] is not None:
+    if stats['strand_bias'] > args.strand_bias:
       filter_out = True
       return filter_out
-  if options.mate_bias and stats['mate_bias'] is not None:
-    if stats['mate_bias'] > options.mate_bias:
+  if args.mate_bias and stats['mate_bias'] is not None:
+    if stats['mate_bias'] > args.mate_bias:
       filter_out = True
       return filter_out
   return filter_out
