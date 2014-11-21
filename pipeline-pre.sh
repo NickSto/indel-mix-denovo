44e5c04ea9713e039ad1ab6842e9ab2503e29a6b
Nick <nmapsy@gmail.com>
2014-11-21 16:25:00 -0500

    pipeline-pre.sh: Simplify code by breaking into functions.

--- pipeline-pre.sh
+++ pipeline-pre.sh
@@ -35,82 +35,17 @@ scriptdir=$(dirname $0)
 
 function main {
 
-  # Check for required commands and scripts
-  for cmd in $REQUIRED_COMMANDS; do
-    if ! which $cmd >/dev/null 2>/dev/null; then
-      fail "Error: could not find $cmd in PATH"
-    fi
-  done
-  for script in $REQUIRED_SCRIPTS; do
-    if [[ ! -f "$scriptdir/$script" ]]; then
-      fail "Error: Required script $script missing from local directory."
-    fi
-  done
-  if [[ ! -d $PICARD_DIR ]] || [[ -z $(ls $PICARD_DIR) ]]; then
-    fail "Picard directory $PICARD_DIR missing or empty."
-  fi
+  # Check for required commands and scripts.
+  check_required "$scriptdir"
 
-  # get options
-  dir=''
-  sample=''
-  chrom="$CHROM_DEFAULT"
-  chim_bounds="$BOUNDS_DEFAULT"
-  while getopts ":s:d:c:B:h" opt; do
-    case "$opt" in
-      s) sample="$OPTARG";;
-      d) dir="$OPTARG";;
-      c) chrom="$OPTARG";;
-      B) chim_bounds="$OPTARG";;
-      h) fail "$USAGE";;
-    esac
-  done
+  # Parse command line arguments.
+  read ref fastq1 fastq2 dir sample chrom bound1 bound2 <<< $(getmyopts "$@")
 
-  # get positional arguments
-  narg=$OPTIND
-  while [[ $narg -le $# ]]; do
-    arg=${@:$narg:1}
-    if [[ ${arg:0:1} == '-' ]]; then
-      fail "Error: options like $arg must come before positional arguments."
-    fi
-    narg=$((narg+1))
-  done
-  positionals=$((narg-OPTIND))
-  if [[ $positionals -lt 3 ]]; then
-    fail "$USAGE"
-  fi
-  ref="${@:$OPTIND:1}"
-  fastq1="${@:$OPTIND+1:1}"
-  fastq2="${@:$OPTIND+2:1}"
+  # Try to determine sample name, if none given.
+  sample=$(get_sample "$sample" "$fastq1")
 
-  # If no -s, try to derive a sample name from the first fastq file.
-  if [[ ! $sample ]]; then
-    sample=$fastq1
-    sample=$(basename $fastq1 .gz)
-    sample=$(basename $sample .fq)
-    sample=$(basename $sample .fastq)
-    sample=$(basename $sample _1)
-    sample=$(basename $sample _2)
-    sample=$(echo "$sample" | sed -E 's/_S[0-9]+_L001_R[12]_001//')
-  fi
-
-  # Check that input files exist
-  #TODO: Check that reference is indexed (avoid unclear BWA error message).
-  for file in $ref $fastq1 $fastq2; do
-    if [[ ! -s $file ]]; then
-      fail "Error: \"$file\" nonexistent, inaccessible, or empty."
-    fi
-  done
-
-  if [[ ! $dir ]]; then
-    dir=$(dirname $fastq1)/$sample
-  fi
-  if [[ -e $dir ]]; then
-    if [[ ! -d $dir ]] || [[ $(ls $dir) ]]; then
-      fail "Error: \"$dir\" is either not a directory or not empty."
-    fi
-  else
-    exho "mkdir -p $dir"
-  fi
+  # Make sure that input files and output directories exist.
+  dir=$(check_paths "$ref" "$fastq1" "$fastq2" "$dir")
 
   echo "\
 scripts: $scriptdir
@@ -131,6 +66,9 @@ sample:  $sample"
   asm_align_raw="$dir/asm_raw.bam"
   asm_align_filt="$dir/asm_filt.bam"
 
+
+  #################### PIPELINE ####################
+
   echo -e "start\t$(date +%s)\t$(date)" > $status
 
   # Align to reference
@@ -138,7 +76,7 @@ sample:  $sample"
 
   # Filter alignment
   exho "bash $scriptdir/pre-process-mt.sh -r $ref -s chimrlen -c $chrom \
-    -B \"$chim_bounds\" $ref_align_raw $ref_align_filt"
+    -B \"$bound1 $bound2\" $ref_align_raw $ref_align_filt"
 
   # Extract reads
   set +e  # Picard exits with spurious errors
@@ -159,8 +97,8 @@ sample:  $sample"
   map $fastq1_filt $fastq2_filt $asm $asm_align_raw $sample
 
   # Filter assembly alignment
-  exho "bash $scriptdir/pre-process-mt.sh -r $ref -s realign -c $chrom -B \"$chim_bounds\" \
-    $asm_align_raw $asm_align_filt"
+  exho "bash $scriptdir/pre-process-mt.sh -r $ref -s realign -c $chrom \
+    -B \"$bound1 $bound2\" $asm_align_raw $asm_align_filt"
 
   #TODO: After this, I need to merge multiple individuals into a family.bam,
   #      then run the actual indel pipeline on that.
@@ -178,12 +116,9 @@ sample:  $sample"
 }
 
 
+# Map fastq's to reference with BWA
 function map {
-  fastq1=$1
-  fastq2=$2
-  ref=$3
-  bam=$4
-  sample=$5
+  read fastq1 fastq2 ref bam sample <<< "$@"
   dir=$(dirname $bam)
   base=$(basename $bam .bam)
   exho "bwa mem -M -t 16 -R '@RG\tID:$sample\tSM:$sample\tPL:$PLATFORM' $ref $fastq1 $fastq2 > $dir/$base.sam"
@@ -191,6 +126,107 @@ function map {
   exho "samtools sort $dir/$base.tmp.bam $dir/$base"
   exho "samtools index $bam"
   exho "rm $dir/$base.sam $dir/$base.tmp.bam"
+}
+
+
+# Read in command line arguments
+function getmyopts {
+  # get options
+  ## Default values can't be empty, otherwise it will be skipped when reading
+  ## output of this function.
+  dir='__NONE__'
+  sample='__NONE__'
+  chrom="$CHROM_DEFAULT"
+  chim_bounds="$BOUNDS_DEFAULT"
+  while getopts ":s:d:c:B:h" opt; do
+    case "$opt" in
+      s) sample="$OPTARG";;
+      d) dir="$OPTARG";;
+      c) chrom="$OPTARG";;
+      B) chim_bounds="$OPTARG";;
+      h) fail "$USAGE";;
+    esac
+  done
+  read bound1 bound2 <<< $(echo $chim_bounds)
+  # get positional arguments
+  narg=$OPTIND
+  while [[ $narg -le $# ]]; do
+    arg=${@:$narg:1}
+    if [[ ${arg:0:1} == '-' ]]; then
+      fail "Error: options like $arg must come before positional arguments."
+    fi
+    narg=$((narg+1))
+  done
+  positionals=$((narg-OPTIND))
+  if [[ $positionals -lt 3 ]]; then
+    fail "$USAGE"
+  fi
+  ref="${@:$OPTIND:1}"
+  fastq1="${@:$OPTIND+1:1}"
+  fastq2="${@:$OPTIND+2:1}"
+  echo "$ref" "$fastq1" "$fastq2" "$dir" "$sample" "$chrom" "$bound1" "$bound2"
+}
+
+
+# Check for required commands and scripts
+function check_required {
+  scriptdir="$1"
+  for cmd in $REQUIRED_COMMANDS; do
+    if ! which $cmd >/dev/null 2>/dev/null; then
+      fail "Error: could not find $cmd in PATH"
+    fi
+  done
+  for script in $REQUIRED_SCRIPTS; do
+    if [[ ! -f "$scriptdir/$script" ]]; then
+      fail "Error: Required script $script missing from local directory."
+    fi
+  done
+  if [[ ! -d $PICARD_DIR ]] || [[ -z $(ls $PICARD_DIR) ]]; then
+    fail "Picard directory $PICARD_DIR missing or empty."
+  fi
+}
+
+
+# If no -s, try to derive a sample name from the first fastq file.
+function get_sample {
+  read sample fastq1 <<< "$@"
+  if [[ $sample == '__NONE__' ]]; then
+    sample=$fastq1
+    sample=$(basename $fastq1 .gz)
+    sample=$(basename $sample .fq)
+    sample=$(basename $sample .fastq)
+    sample=$(basename $sample _1)
+    sample=$(basename $sample _2)
+    sample=$(echo "$sample" | sed -E 's/_S[0-9]+_L001_R[12]_001//')
+  fi
+  echo "$sample"
+}
+
+
+# Make sure that input files and output directories exist.
+function check_paths {
+  read ref fastq1 fastq2 dir <<< "$@"
+  #TODO: Check that reference is indexed (avoid unclear BWA error message).
+  # Check for existence of input files.
+  for file in $ref $fastq1 $fastq2; do
+    if [[ ! -s $file ]]; then
+      fail "Error: \"$file\" nonexistent, inaccessible, or empty."
+    fi
+  done
+  # Check $dir.
+  # If not given with -d, determine it from the fastq1 path.
+  # Make sure if it exists, it's empty.
+  if [[ $dir == '__NONE__' ]]; then
+    dir=$(dirname $fastq1)/$sample
+  fi
+  if [[ -e $dir ]]; then
+    if [[ ! -d $dir ]] || [[ $(ls $dir) ]]; then
+      fail "Error: \"$dir\" is either not a directory or not empty."
+    fi
+  else
+    exho "mkdir -p $dir" >&2
+  fi
+  echo "$dir"
 }
 
 
