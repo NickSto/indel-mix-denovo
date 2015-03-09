1f0c9af2cdbd2ae7407c6d3b7e0df91ac4576a35
me <nmapsy@gmail.com>
2015-03-09 15:32:38 -0400

    tests/run.sh: Add pipeline step 3 test (and exit if error creating tmp dir).

--- tests/run.sh
+++ tests/run.sh
@@ -20,6 +20,8 @@ function main {
         pipeline;;
       pipeline2)
         pipeline2;;
+      pipeline3)
+        pipeline3;;
     esac
   done
 }
@@ -30,18 +32,33 @@ function main {
 # Do all tests of the pipeline.
 function pipeline {
   pipeline2
+  pipeline3
 }
 
 # Test up through step 2 of the pipeline.
 function pipeline2 {
-  echo -e "\tpipeline.py -E 2 ::: R39-M249-reduced.bam:"
-  mkdir "$dirname/pipeline/tmp"
+  echo -e "\tpipeline.py steps 1 and 2 ::: R39-M249-reduced.bam:"
+  mkdir "$dirname/pipeline/tmp" || return
   python "$dirname/../pipeline.py" -E 2 -s M249 -R chrM "$dirname/chrM-rCRS.fa" \
     "$dirname/pipeline/R39-M249-reduced_1.fq" "$dirname/pipeline/R39-M249-reduced_2.fq" \
     "$dirname/pipeline/tmp" 2>/dev/null
-  #TODO: edit diff.sh to allow different paths in @PG command line
-  bash "$dirname/diff.sh" "$dirname/pipeline/R39-M249-reduced.bam.out2" \
+  #TODO: edit diff.sh to allow different paths in @PG command line.
+  bash "$dirname/diff.sh" "$dirname/pipeline/out2.R39-M249-reduced.bam" \
     "$dirname/pipeline/tmp/bam1filt.bam"
+  rm -r "$dirname/pipeline/tmp"
+}
+
+# Test step 3 of the pipeline.
+function pipeline3 {
+  echo -e "\tpipeline.py step 3 ::: R39-M249-reduced.bam:"
+  mkdir "$dirname/pipeline/tmp" || return
+  cp "$dirname/pipeline/out2.R39-M249-reduced.bam" "$dirname/pipeline/tmp/bam1filt.bam"
+  python "$dirname/../pipeline.py" -B 3 -E 3 -s M249 -R chrM "$dirname/chrM-rCRS.fa" \
+    "$dirname/pipeline/R39-M249-reduced_1.fq" "$dirname/pipeline/R39-M249-reduced_2.fq" \
+    "$dirname/pipeline/tmp" 2>/dev/null
+  #TODO: edit diff.sh to allow different paths in @PG command line.
+  bash "$dirname/diff.sh" "$dirname/pipeline/out3.R39-M249-reduced.bam" \
+    "$dirname/pipeline/tmp/bam1dedup.bam"
   rm -r "$dirname/pipeline/tmp"
 }
 
