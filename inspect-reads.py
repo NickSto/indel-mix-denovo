49062e15d4c6fb6eff8b24c65309c2a4fda7eb89
me <nmapsy@gmail.com>
2015-04-24 12:20:51 -0400

    inspect-reads.py: Take out unimplemented options.

--- inspect-reads.py
+++ inspect-reads.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python
 # requires Python 2.7
-"""The guts of the read selection and statistics calculation will be done by the
-bamslicer module, so that it can also be used by nvc-filter.py. Then
-nvc-filter.py can use the statistics as a filter."""
+# The guts of the read selection and statistics calculation will be done by the
+# bamslicer module, so that it can also be used by nvc-filter.py. Then
+# nvc-filter.py can use the statistics as a filter.
 from __future__ import division
-import os
 import sys
 import argparse
 import collections
 import simplewrap
 import vcfreader
 import bamslicer
-# ./inspect-reads.py ~/backuphide/bfx/R19S5-new-nm.bam -H -v chrM-R19S5-new-nm-dedup:15873-I,chrM-R19S5-new-nm-dedup:15873-D,chrM-R19S5-new-nm-dedup:13718-D,chrM-R19S5-new-nm-dedup:11571-D,chrM-R19S5-new-nm-dedup:3757-D
-# ./inspect-reads.py tests/cigar-tests.bam -t -v chrM:5-I,chrM:199-D,chrM:199-I,chrM:3106-D,chrM:6110-D,chrM:16568-I
 
-EXPECTED_VERSIONS = {'simplewrap':'0.6', 'vcfreader':'0.51', 'bamslicer':'0.6'}
+EXPECTED_VERSIONS = {'vcfreader':'0.51', 'bamslicer':'0.6', 'simplewrap':'0.6'}
 
-"""Need to use argparse.RawTextHelpFormatter to preserve formatting in the
-description of columns in the tsv output. But to still accommodate different
-terminal widths, dynamic wrapping with simplewrap will be necessary."""
+# Need to use argparse.RawTextHelpFormatter to preserve formatting in the
+# description of columns in the tsv output. But to still accommodate different
+# terminal widths, dynamic wrapping with simplewrap will be necessary.
 WIDTH = simplewrap.termwidth(70)
 wrapper = simplewrap.Wrapper(width=WIDTH)
 wrap = wrapper.wrap
@@ -27,14 +24,8 @@ OPT_DEFAULTS = {'human':True, 'tsv':False, 'vartypes':'ID',
   'strand_bias':0, 'mate_bias':0}
 USAGE = "%(prog)s [options] -v variantslist -V variants.vcf reads.bam"
 DESCRIPTION = wrap("Retrieve the reads supporting a given set of "
-  "variants, and report statistics on them. Provide the variants in a VCF "
+  "indels, and report statistics on them. Provide the variants in a VCF "
   "and/or in a list on the command line.")
-EPILOG = wrap("WARNING: Work in progress. Not yet implemented features:\n"
-  "--containing --output-bam --all --opposing options\n"
-  "Considering the \"alt\" information of variants (the only thing considered "
-  "right now is the location and type).\n"
-  "And most important of all, ***SNVS ARE NOT SUPPORTED!*** Right now only "
-  "indels are recognized.")
 
 VALID_BASES = 'GATCNgatcn'
 LABELS = """Sample Chrom Coord Type Alt Covg Reads Freq Unmap Improp Fwd First
@@ -48,7 +39,7 @@ def main():
   wrapper.width = WIDTH-24
 
   parser = argparse.ArgumentParser(usage=USAGE, description=DESCRIPTION,
-    epilog=EPILOG, formatter_class=argparse.RawTextHelpFormatter)
+    formatter_class=argparse.RawTextHelpFormatter)
   parser.set_defaults(**OPT_DEFAULTS)
 
   parser.add_argument('bamfilepath', metavar='reads.bam',
@@ -60,12 +51,13 @@ def main():
       '"type" is "S", "I", or "D", for SNV, insertion, or deletion. "alt" is '
       'optional, and specifies the alternate allele: the SNV alternate base, '
       'the length of the deletion, or the inserted sequence (including the '
-      'preceding base, as in VCF). If "alt" is not provided, it will select '
-      'any read with that type of variant at that location.'))
+      'preceding base, as in VCF). At the moment, this is not considered, and '
+      'it will select any read with that type of variant at that location.'))
   parser.add_argument('-V', '--vcf',
     help='Use the variants in the ALT column of this VCF file.')
-  parser.add_argument('-o', '--output-bam',
-    help='Output the selected reads to this BAM file.')
+  #TODO:
+  # parser.add_argument('-o', '--output-bam',
+  #   help='Output the selected reads to this BAM file.')
   parser.add_argument('-H', '--human', action='store_true',
     help='Print statistics in a human-readable format (default).')
   parser.add_argument('-t', '--tsv', action='store_true',
@@ -116,23 +108,24 @@ def main():
   parser.add_argument('-S', '--sample-name', metavar='NAME',
     help=wrap('Label all output with this sample name (the first tsv column), '
       'ignoring any read group data from the input BAM.'))
-  parser.add_argument('-T', '--vartypes', metavar='TYPES',
-    help=wrap('Only consider these variant types. Give a string of letters, '
-      'e.g. "SID" to keep SNVs (S), insertions (I), and deletions (D). '
-      'Default: "%(default)s"'))
-  parser.add_argument('-a', '--all', action='store_true',
-    help=wrap('Select all the reads covering the variants, not just those '
-      'supporting the variant. The statistics reported will be on these reads, '
-      'not the supporting reads.'))
-  parser.add_argument('-O', '--opposing', action='store_true',
-    default=OPT_DEFAULTS.get('opposing'),
-    help=wrap('Select the reads OPPOSING the variants. The statistics reported '
-      'will be on these reads, not the supporting reads.'))
-  parser.add_argument('-C', '--containing', action='store_true',
-    help=wrap('For deletions, select any read with a deletion which *contains* '
-      'the specified one. So if chr1:2345-D:2 is a variant of interest, and a '
-      'read has the variant chr1:2344-D:3, it will select that read even '
-      'though the variants don\'t have the same start coordinates.'))
+  #TODO:
+  # parser.add_argument('-T', '--vartypes', metavar='TYPES',
+  #   help=wrap('Only consider these variant types. Give a string of letters, '
+  #     'e.g. "SID" to keep SNVs (S), insertions (I), and deletions (D). '
+  #     'Default: "%(default)s"'))
+  # parser.add_argument('-a', '--all', action='store_true',
+  #   help=wrap('Select all the reads covering the variants, not just those '
+  #     'supporting the variant. The statistics reported will be on these reads, '
+  #     'not the supporting reads.'))
+  # parser.add_argument('-O', '--opposing', action='store_true',
+  #   default=OPT_DEFAULTS.get('opposing'),
+  #   help=wrap('Select the reads OPPOSING the variants. The statistics reported '
+  #     'will be on these reads, not the supporting reads.'))
+  # parser.add_argument('-C', '--containing', action='store_true',
+  #   help=wrap('For deletions, select any read with a deletion which *contains* '
+  #     'the specified one. So if chr1:2345-D:2 is a variant of interest, and a '
+  #     'read has the variant chr1:2344-D:3, it will select that read even '
+  #     'though the variants don\'t have the same start coordinates.'))
   parser.add_argument('-s', '--strand-bias', type=float, metavar='SBIAS',
     help=wrap('Filter out variants with a strand bias value greater than or '
       'equal to this. The strand bias statistic is method 1 (SB) of Guo et '
@@ -153,6 +146,9 @@ def main():
     parser.print_help()
     fail("\nError: Please provide a list of variants in either a VCF file or a "
          "command line option.")
+
+  # shim for planned option
+  setattr(args, 'vartypes', 'ID')
   # eliminate variants that aren't one of the specified types
   variants[:] = [var for var in variants if var['type'] in args.vartypes]
 
