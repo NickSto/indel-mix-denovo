2c8c37c63a4cb690617e8140d22c6580795ebbb7
me <nmapsy@gmail.com>
2015-05-11 16:28:54 -0400

    heteroplasmy/edge_effect.py: Add version check for incompatible 0.8.1 version.

--- heteroplasmy/edge_effect.py
+++ heteroplasmy/edge_effect.py
@@ -7,6 +7,7 @@ import os
 import sys
 import pysam
 import argparse
+import distutils.version
 
 OPT_DEFAULTS = {'chrom':'chrM', 'min_qual':30, 'edge_size':25}
 
@@ -16,6 +17,18 @@ the variant appears in edge of the read."""
 
 
 def main():
+
+  # Broken by PySAM 0.8.1.
+  # Fails with error:
+  # AttributeError: 'pysam.calignmentfile.PileupRead' object has no attribute 'qpos'
+  # on line:
+  #             read.alignment.seq[read.qpos] == minor and
+  # Looks they probably renamed qpos to something like query_alignment_position, though they don't
+  # specifically mention it in the release notes:
+  # http://pysam.readthedocs.org/en/latest/release.html#release-0-8-1
+  version = pysam.__version__
+  if distutils.version.StrictVersion(version) >= distutils.version.StrictVersion('0.8.1'):
+    raise Exception('Not compatible with PySAM versions > 0.8.0. Current version: '+version)
 
   parser = argparse.ArgumentParser(description=DESCRIPTION)
   parser.set_defaults(**OPT_DEFAULTS)
